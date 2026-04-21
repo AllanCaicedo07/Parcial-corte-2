036cba6205a2fae66b35a3f5128b234f456cd146
Allan Caicedo <amatthewcaicedo@estudiante.uniajc.edu.co>
2026-04-20 20:46:20 -0500

    se crea el archivo de MAgo.java

---
+++ src/main/java/parcial/corte2/Mago.java
@@ -0,0 +1,127 @@
+package parcial.corte2;
+
+public class Mago extends Criatura implements Magico{
+    
+    //atributos específicos del Mago
+    private String hechizos;
+    private Arma arma;
+ 
+    /**
+     * Constructor del Mago.
+     *
+     * @param nombre   Nombre del mago
+     * @param salud    Puntos de salud iniciales
+     * @param fuerza   Fuerza base del mago
+     * @param hechizos Hechizo inicial que conoce el mago
+     */
+    public Mago(String nombre, int salud, int fuerza, String hechizos) {
+        super(nombre, salud, fuerza);
+        this.hechizos = hechizos;
+        this.arma = null;
+    }
+ 
+    /**
+     * Ataca a una criatura objetivo usando hechizos.
+     * El mago aplica su fuerza base como daño.
+     * Si tiene un arma equipada, se suma el daño adicional del arma.
+     *
+     * @param objetivo La criatura que recibirá el ataque
+     */
+    @Override
+    public void atacar(Criatura objetivo) {
+        int dañoTotal = fuerza;
+ 
+        lanzarHechizo(); // El mago siempre lanza un hechizo al atacar
+ 
+        if (arma != null) {
+            System.out.println(nombre + " ataca también con su arma: " + arma.getNombre() + "!");
+            arma.atacarConArma(objetivo);
+            dañoTotal += arma.getDañoAdicional();
+        }
+ 
+        System.out.println(nombre + " ataca a " + objetivo.getNombre()
+                + " con el hechizo \"" + hechizos + "\"! Daño: " + dañoTotal);
+        objetivo.defender(dañoTotal);
+    }
+ 
+    /**
+     * El mago se defiende de un ataque recibido.
+     * Usa un escudo mágico que reduce el daño recibido en un 10%.
+     *
+     * @param daño Cantidad de daño recibido
+     */
+    @Override
+    public void defender(int daño) {
+        int dañoReducido = (int) (daño * 0.9); // El escudo mágico absorbe un 10% del daño
+        salud -= dañoReducido;
+        System.out.println(nombre + " conjura un escudo mágico! Recibe "
+                + dañoReducido + " de daño. Salud restante: " + salud);
+    }
+ 
+    
+    // El mago lanza su hechizo actual.
+    // Implementación del método de la interfaz Magico.
+     
+    @Override
+    public void lanzarHechizo() {
+        System.out.println(nombre + " lanza el hechizo: \"" + hechizos + "\"!");
+    }
+ 
+    
+     // El mago aprende un nuevo hechizo, reemplazando el anterior.
+     // Implementación del método de la interfaz Magico.
+     
+    @Override
+    public void aprenderHechizo() {
+        String nuevoHechizo = "Rayo Arcano"; // Hechizo nuevo que aprende por defecto
+        System.out.println(nombre + " ha aprendido un nuevo hechizo: \"" + nuevoHechizo + "\"!");
+        this.hechizos = nuevoHechizo;
+    }
+ 
+    /**
+     * Sobrecarga de aprenderHechizo para aprender un hechizo específico.
+     *
+     * @param nuevoHechizo El nombre del nuevo hechizo a aprender
+     */
+    public void aprenderHechizo(String nuevoHechizo) {
+        System.out.println(nombre + " ha aprendido un nuevo hechizo: \"" + nuevoHechizo + "\"!");
+        this.hechizos = nuevoHechizo;
+    }
+ 
+    /**
+     * Equipa un arma al mago (composición).
+     *
+     * @param arma El arma a equipar
+     */
+    public void equiparArma(Arma arma) {
+        this.arma = arma;
+        System.out.println(nombre + " ha equipado: " + arma.getNombre());
+    }
+ 
+    /**
+     * Desequipa el arma actual del mago.
+     */
+    public void desequiparArma() {
+        if (arma != null) {
+            System.out.println(nombre + " ha desequipado: " + arma.getNombre());
+            this.arma = null;
+        } else {
+            System.out.println(nombre + " no tiene ningún arma equipada.");
+        }
+    }
+ 
+    // -------------------- Getters y Setters --------------------
+ 
+    public String getHechizos() {
+        return hechizos;
+    }
+ 
+    public void setHechizos(String hechizos) {
+        this.hechizos = hechizos;
+    }
+ 
+    public Arma getArma() {
+        return arma;
+    }
+
+}
